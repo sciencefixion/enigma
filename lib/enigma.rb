c5f3e70ae2083d77d971217adb6ccd931f8deac7
Rion Hutsell <st.gish@gmail.com>
2020-06-06 14:52:44 -0600

    Add initialize and random_five_digit_num methods with tests

---
+++ lib/enigma.rb
@@ -0,0 +1,9 @@
+class Enigma
+
+  def initialize
+  end
+
+  def random_five_digit_num
+    rand.to_s[2..6]
+  end
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_random_five_digit_num
+
+    assert_equal 5, @enigma.random_five_digit_num.length
+  end
+  def generate_keys
+    skip
+
+    assert_instance_of Hash, @enigma.generate_keys
+    assert_equal 4, @enigma.generate_keys.length
+  end
 end
