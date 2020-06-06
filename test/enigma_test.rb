725e5245121bf13355dfcd11a89f049ca1958831
Rion Hutsell <st.gish@gmail.com>
2020-06-06 14:18:12 -0600

    Add EnigmaTest and Enigma class files

---
+++ test/enigma_test.rb
@@ -0,0 +1,15 @@
+require './test/setup'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+
+  def setup
+  @enigma = Enigma.new
+  end
+
+  def test_it_exists
+
+    assert_instance_of Enigma, @enigma
+  end
+
+end
