6a7b0adb7d7eebf357a4aae1f966c0872c6ebc86
Rion Hutsell <st.gish@gmail.com>
2020-06-08 17:55:50 -0600

    Add file read/write functionality to decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,3 +4,19 @@ file = ARGV[0]
 file_2 = ARGV[1]
 key = ARGV[2]
 date = ARGV[3]
+
+encrypted = File.open(file, 'r')
+msg = encrypted.read().strip
+
+
+outfile = File.new("#{file_2}", 'w')
+outfile.puts("#{msg}")
+
+encrypted.close()
+outfile.close()
+
+puts "Created '#{file_2}' with key and date"
+
+
+# $ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
+# Created 'decrypted.txt' with the key 82648 and date 240818
