97a99903eca4a1405c2f36840716d92894ebdf20
Rion Hutsell <st.gish@gmail.com>
2020-06-08 17:48:50 -0600

    Add Enigma#encrypt to encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,11 +5,16 @@ file_2 = ARGV[1]
 
 message = File.open(file, 'r')
 msg = message.read().strip
+enigma = Enigma.new
+encrypted = enigma.encrypt(msg)
 
 outfile = File.new("#{file_2}", 'w')
-outfile.puts("#{msg}")
+outfile.puts("#{encrypted[:encryption]}")
 
 message.close()
 outfile.close()
 
-puts "Created '#{file_2}' with key_ and date_}"
+puts "Created '#{file_2}' with key #{encrypted[:key]} and date #{encrypted[:date]}"
+
+# $ ruby ./lib/encrypt.rb message.txt encrypted.txt
+# Created 'encrypted.txt' with the key 82648 and date 240818
