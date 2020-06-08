cf047f4ead86981703b933d75fc65e690c09ca0b
Rion Hutsell <st.gish@gmail.com>
2020-06-08 17:39:11 -0600

    Add file read funtionality to encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,4 +3,7 @@ require './lib/enigma'
 file = ARGV[0]
 file_2 = ARGV[1]
 
-p "#{file} and #{file_2}"
+message = File.open(file, 'r')
+p message.read().strip
+
+puts "Created '#{file_2}' with key_ and date_}"
