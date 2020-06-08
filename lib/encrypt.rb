25fc71032f4f303b7f8329b68fcb4ad8a97b54c1
Rion Hutsell <st.gish@gmail.com>
2020-06-08 17:44:35 -0600

    Add file write funtionality to encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,6 +4,12 @@ file = ARGV[0]
 file_2 = ARGV[1]
 
 message = File.open(file, 'r')
-p message.read().strip
+msg = message.read().strip
+
+outfile = File.new("#{file_2}", 'w')
+outfile.puts("#{msg}")
+
+message.close()
+outfile.close()
 
 puts "Created '#{file_2}' with key_ and date_}"
